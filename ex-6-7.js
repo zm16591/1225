f92f22752be7449c280474ef4c62d29ba89e7524
zm16591 <zm16591@josai.ac.jp>
2018-07-29 21:31:10 -0400

    message

---
+++ ex-6-7.js
@@ -0,0 +1,8 @@
+var who ='world';
+function sayhello(){
+  alert('Hello,'+who+'!');
+}
+function someone(x){
+  who = x;
+  
+}
---
+++ myjs.js
@@ -0,0 +1,4 @@
+
+alert('Hello,Minami!');
+alert(1+1);
+alert(Math.sqrt(1+1));
